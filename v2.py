58927a2f91578bd30282d26e43732055acbce856
Obie Smolenski <mr.obadiah@gmail.com>
2021-11-14 17:30:51 -0500

    Final v2 real

--- v2.py
+++ v2.py
@@ -2,6 +2,8 @@ from pyautogui import *
 from PIL import Image
 import time
 
+START_TIME = [time.time(), 0]
+
 BACKGROUND_COLOR = (149, 191, 219)
 LANE_COLOR = (163, 200, 224)
 DEAD_COLORS = ((30, 38, 44), (12, 13, 18))
@@ -9,8 +11,11 @@ COIN_COLORS = ((72, 54, 0), (249, 255, 220), (255, 247, 28), (255, 134, 32), (18
 PURPLE_COIN_COLORS = ((32, 0, 72), (250, 220, 255), (179, 28, 255), (65, 0, 185), (126, 32, 255), (146, 126, 255))
 ROCK_COLORS = ((62, 67, 88), (87, 94, 124), (160, 165, 188), (124, 131, 163))
 COLORS = (BACKGROUND_COLOR, LANE_COLOR, DEAD_COLORS, COIN_COLORS, PURPLE_COIN_COLORS, ROCK_COLORS)
+
 lane = 1
+
 width, height = size()
+
 # center of boat is at 9*height/10
 obstacleCheckHeight = 0.72 * height # should be between 0.6 - 0.75
 coinCheckHeight = 7*height/10 # my favorite number is 72. did you know that?
@@ -24,14 +29,14 @@ def findObstacle(lane, mode=0):
     if not mode:
         px = readPixel(lane, obstacleCheckHeight)
         if px in ROCK_COLORS: # rock is in lane i
-            obstacleCheckHeight -= inc
+            changeCheckHeight()
             return True
         return False # no rock
     else:
         global coinCheckHeight
         px = readPixel(lane, coinCheckHeight)
         if px in ROCK_COLORS:
-            obstacleCheckHeight -= inc
+            changeCheckHeight()
             return True
     return False
 
@@ -50,7 +55,7 @@ def findCoin():
             move("left", 2)
         """elif px in PURPLE_COIN_COLORS:
             move("left", 3)""" # moving over three lanes doesn't really work
-        return 0
+        #return 0
     
     # check lane 1
     px = readPixel(1, coinCheckHeight)
@@ -61,9 +66,9 @@ def findCoin():
             pass # no movement necessary
         elif lane == 2:
             move("left")
-        elif px in PURPLE_COIN_COLORS:
+        elif lane == 3 and px in PURPLE_COIN_COLORS:
             move("left", 2)
-        return 1
+        #return 1
     
     # check lane 2
     px = readPixel(2, coinCheckHeight)
@@ -76,7 +81,7 @@ def findCoin():
             pass # no movement necessary
         elif lane == 3:
             move("left")
-        return 2
+        #return 2
     
     # check lane 3
     px = readPixel(3, coinCheckHeight)
@@ -89,12 +94,31 @@ def findCoin():
             move("right")
         elif lane == 3:
             pass # no movement necessary
-        return 3
+        #return 3
     
-    # end script if you die - this function gets called the least
+    # end script if you die
     if px in DEAD_COLORS:
         raise KeyboardInterrupt
-    return -1
+    #return -1
+
+def changeCheckHeight():
+    global START_TIME, inc, obstacleCheckHeight
+    
+    if START_TIME[1] == 0 and time.time() - START_TIME[0] >= 20:
+        inc += 2
+        START_TIME[1] += 1
+    elif START_TIME[1] == 1 and time.time() - START_TIME[0] >= 30:
+        inc += 2
+        START_TIME[1] += 1
+    elif START_TIME[1] == 2 and time.time() - START_TIME[0] >= 35:
+        inc += 2
+        START_TIME[1] += 1
+    elif START_TIME[1] == 3 and time.time() - START_TIME[0] >= 40:
+        inc += 2
+        START_TIME[1] += 1
+    
+    obstacleCheckHeight -= inc
+    print(inc)
 
 def readPixel(lane, checkHeight):
     global lastObstaclePixel
@@ -138,7 +162,7 @@ try:
                 move("right")
             else:
                 move()
-        else:
+        elif time.time() - START_TIME[0] <= 30:
             findCoin()
 except KeyboardInterrupt:
         pass
